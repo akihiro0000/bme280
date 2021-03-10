df28958431f9fada9260b5d06823b671b000e107
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-03-10 14:22:10 +0900

    Update run.py

--- run.py
+++ run.py
@@ -111,7 +111,7 @@ def compensate_P(adc_P):
 	v2 = ((pressure / 4.0) * digP[7]) / 8192.0
 	pressure = pressure + ((v1 + v2 + digP[6]) / 16.0)  
 
-	return pressure
+	return round(pressure,3)
 
 def compensate_T(adc_T):
 	global t_fine
@@ -119,7 +119,7 @@ def compensate_T(adc_T):
 	v2 = (adc_T / 131072.0 - digT[0] / 8192.0) * (adc_T / 131072.0 - digT[0] / 8192.0) * digT[2]
 	t_fine = v1 + v2
 	temperature = t_fine / 5120.0
-	return  temperature
+	return  round(temperature,3)
 
 def compensate_H(adc_H):
 	global t_fine
@@ -133,7 +133,7 @@ def compensate_H(adc_H):
 		var_h = 100.0
 	elif var_h < 0.0:
 		var_h = 0.0
-	return var_h
+	return round(var_h,3)
 
 
 def setup():
